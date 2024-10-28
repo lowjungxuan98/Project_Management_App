b18f95fe0adc3e334031f6fb6f42869afb4b9c24
lowjungxuan <lowjungxuan@gmail.com>
2024-10-28 16:52:12 +0800

    update swagger.ts

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -16,12 +16,123 @@ const doc = {
       description: "Production API Doc",
     },
     {
-      url: "https://i3p592k4x2.execute-api.ap-southeast-1.amazonaws.com/prod",
-      description: "API Gateway"
+      url: "https://i3p592k4x2.execute-api.ap-southeast-1.amazonaws.com/{basePath}",
+      variables: {
+        basePath: {
+          default: "prod"
+        }
+      }
     }
-
   ],
+  paths: {
+    "/{proxy+}": {
+      options: {
+        parameters: [
+          {
+            name: "proxy",
+            in: "path",
+            required: true,
+            schema: {
+              type: "string"
+            }
+          }
+        ],
+        responses: {
+          "200": {
+            description: "200 response",
+            headers: {
+              "Access-Control-Allow-Origin": {
+                schema: {
+                  type: "string"
+                }
+              },
+              "Access-Control-Allow-Methods": {
+                schema: {
+                  type: "string"
+                }
+              },
+              "Access-Control-Allow-Headers": {
+                schema: {
+                  type: "string"
+                }
+              }
+            },
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Empty"
+                }
+              }
+            }
+          }
+        },
+        "x-amazon-apigateway-integration": {
+          responses: {
+            default: {
+              statusCode: "200",
+              responseParameters: {
+                "method.response.header.Access-Control-Allow-Methods": "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
+                "method.response.header.Access-Control-Allow-Headers": "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'",
+                "method.response.header.Access-Control-Allow-Origin": "'*'"
+              }
+            }
+          },
+          requestTemplates: {
+            "application/json": "{\"statusCode\": 200}"
+          },
+          passthroughBehavior: "when_no_match",
+          type: "mock"
+        }
+      },
+      "x-amazon-apigateway-any-method": {
+        parameters: [
+          {
+            name: "proxy",
+            in: "path",
+            required: true,
+            schema: {
+              type: "string"
+            }
+          }
+        ],
+        responses: {
+          "200": {
+            description: "200 response",
+            content: {
+              "application/json": {
+                schema: {
+                  $ref: "#/components/schemas/Empty"
+                }
+              }
+            }
+          }
+        },
+        "x-amazon-apigateway-integration": {
+          httpMethod: "ANY",
+          uri: "http://18.141.160.220/{proxy}",
+          responses: {
+            default: {
+              statusCode: "200"
+            }
+          },
+          requestParameters: {
+            "integration.request.path.proxy": "method.request.path.proxy"
+          },
+          passthroughBehavior: "when_no_match",
+          cacheNamespace: "mbcs5j",
+          cacheKeyParameters: ["method.request.path.proxy"],
+          type: "http_proxy"
+        }
+      }
+    }
+  },
   components: {
+    schemas: {
+      Empty: {
+        title: "Empty Schema",
+        type: "object"
+      }
+    },
     securitySchemes: {
       bearerAuth: {
         type: "http",
